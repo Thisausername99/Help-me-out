3e6b9fed3c7ab570e694e84923322d823f4e8b0d
Thisausername99 <cody_hoang99@yahoo.com>
2021-09-05 21:06:42 -0700

    Removed folder from repository

--- codyweb-content-service/src/main/java/com/international/codyweb/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.international.codyweb.model.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.international.codyweb.model.entity.UserEntity;
-//
-//public interface UserRepository extends JpaRepository<UserEntity,Long> {
-//
-//}
--- codyweb-content-service/src/test/java/com/international/codyweb/EmailApplicationTest.java
+++
@@ -1,8 +0,0 @@
-package com.international.codyweb;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class EmailApplicationTest {
-
-}
